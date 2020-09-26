6c7128a9919f3eae22e0c0c74229d57de25930b8
Peter Wauyo <pbwauyo@gmail.com>
2020-09-27 00:50:47 +0300

    fixed bugs in layout

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
      path('login/',views.login_view ,name='login'),
-     path('signup/', views.signup_view, name='signup')
+     path('signup/', views.signup_view, name='signup'),
+     path('logout/',views.logout_view ,name='logout'),
 ]
